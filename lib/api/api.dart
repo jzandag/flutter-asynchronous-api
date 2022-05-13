b2a9c58eb46016670b08f5c09ca5447b6af1eb1f
jzandag <zidrexandag10@gmail.com>
2022-05-13 20:17:17 +0800

    added api keys and http dependency

---
+++ lib/api/api.dart
@@ -0,0 +1,6 @@
+class ApiHelper {
+  static const Map<String, String> headers = {
+    'X-RapidAPI-Host': 'shazam.p.rapidapi.com',
+    'X-RapidAPI-Key': 'edbcbb97aemsh9d06aed841252b3p190530jsnc75bf6a704fa'
+  };
+}
